3cde03a4bb7071322440946670ee28b6fa80d862
Mustafa <mustafa.khan@datamato.com>
2025-01-13 16:09:22 +0530

    feat: add httpserver to test deployment

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"sync"
 
-	"github.com/MUSTAFA-A-KHAN/funny-telegram-bot/controller"
+	"github.com/MUSTAFA-A-KHAN/funny-telegram-bot/controller/joker"
 )
 
 func main() {
 	botToken := "7484235929:AAHiFUWLo2nmqXyMe9gby7yc0SBUb8ZysE4"
-	err := controller.StartBot(botToken)
+	var wg sync.WaitGroup
+	// Add two tasks to the WaitGroup
+	wg.Add(2)
+	go runJokerBot(botToken, &wg)
+	go startHTTPServer()
+	// Wait for goroutines to complete
+	wg.Wait()
+
+	log.Println("All bots stopped running.")
+
+}
+
+// Function to start a bot with error handling
+func runJokerBot(botToken string, wg *sync.WaitGroup) {
+	defer wg.Done() // Decrement the wait group counter when this goroutine completes
+	err := joker.StartBot(botToken)
 	if err != nil {
-		log.Panic("error strting bot: ", err)
+		log.Panic("error starting bot: ", err)
 	}
+
+}
+
+// startHTTPServer starts a simple HTTP server for health checks
+func startHTTPServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Bot is running!")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
